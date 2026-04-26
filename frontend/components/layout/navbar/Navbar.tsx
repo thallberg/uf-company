d4fd18b0024a422e0753209686c8caf255eae6ff
thallberg <t.hallberg84@gmail.com>
2026-04-26 12:16:44 +0200

    Refactor Navbar and Footer components for improved styling consistency and accessibility

--- frontend/components/layout/navbar/Navbar.tsx
+++ frontend/components/layout/navbar/Navbar.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { ShoppingCartButton } from "@/components/features/cart/ShoppingCartButton"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ShoppingCartButton } from "@/components/features/cart/ShoppingCartButton";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { House } from "lucide-react";
 
 export function Navbar() {
   return (
     <nav className="border-b bg-brand-navbar h-15 sticky top-0 z-50">
       <div className="container mx-auto text-brand-white flex h-14 items-center justify-between px-4">
-
         {/* LEFT */}
         <div className="flex items-center gap-2">
           {/* 🔥 sidebar toggle */}
@@ -20,8 +20,8 @@ export function Navbar() {
             variant="link"
             className="text-brand-white hover:text-brand-yellow"
           >
-            <Link href="/" className="font-bold text-lg">
-              UF Store
+            <Link aria-label="Home" href="/" className="p-2 rounded-md bg-white/20 hover:bg-brand-yellow-solid transition">
+              <House className="w-5! h-5! text-brand-white"  />
             </Link>
           </Button>
         </div>
@@ -30,8 +30,7 @@ export function Navbar() {
         <div className="md:flex items-center gap-4">
           <ShoppingCartButton />
         </div>
-
       </div>
     </nav>
-  )
+  );
 }
